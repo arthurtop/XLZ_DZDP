d028b316ff4400acb43c72eb657c92c1fc6906f9
goddess <arthurtop@163.com>
2016-09-28 14:07:35 +0800

    集成 cocoapods
    将捂脸天下 从新用swift写

---
+++ XLZ_DZDingPing/XLZ_DZDingPing/Class/BaseClassVC/BaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseViewController.swift
+//  XLZ_DZDingPing
+//
+//  Created by jecansoft on 16/9/28.
+//  Copyright © 2016年 zyhl. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
